08586f541dbb4e7a6c2c658d014717121fb54932
AdonNeet <baradontap@gmail.com>
2024-06-26 09:34:45 +0700

    chore: update home view

---
+++ app/views/home/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BuBar</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .navbar-nav .nav-link {
+            color: #000;
+        }
+        .jumbotron {
+            background-color: #FFA500;
+            color: #000;
+        }
+        .about-us, .faq {
+            padding: 60px 0;
+        }
+        .footer {
+            background-color: #f8f9fa;
+            padding: 20px 0;
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <a class="navbar-brand" href="#">BuBar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="#main">Beranda</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Books</a></li>
+            <li class="nav-item"><a class="nav-link" href="#about-us">About Us</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Kategori</a></li>
+        </ul>
+    </div>
+</nav>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,10 +1,111 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Home</title>
-    <link rel="stylesheet" type="text/css" href="/css/styles.css">
-</head>
-<body>
-    <h1>Welcome to the Home Page</h1>
-</body>
-</html>
+<?php include('header.php'); ?>
+
+<div class="jumbotron text-center" id="main">
+    <h1 class="display-4">OPEN YOUR WORLD</h1>
+    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">Discover More</a>
+</div>
+
+<div class="container" id="bubar-section">
+    <h2 class="text-center my-4">BuBar</h2>
+    <p>BuBar adalah sebuah website yang dibuat bertujuan untuk memudah kita dalam meminjam buku tanpa harus datang dan mengantri. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+
+<div class="about-us text-center" id="about-us">
+    <h2>ABOUT US</h2>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="app\views\home\dummy.jpg" class="card-img-top" alt="Foto">
+                <div class="card-body">
+                    <p class="card-text">Nama: As'ad Nirot Ahmadi</p>
+                    <p class="card-text">NIM: L200220155</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="#" class="card-img-top" alt="Foto">
+                <div class="card-body">
+                    <p class="card-text">Nama: As'ad Nirot Ahmadi</p>
+                    <p class="card-text">NIM: L200220155</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="app\views\home\dummy.jpg" class="card-img-top" alt="Foto">
+                <div class="card-body">
+                    <p class="card-text">Nama: As'ad Nirot Ahmadi</p>
+                    <p class="card-text">NIM: L200220155</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="faq container" id="faq-section">
+    <h2 class="text-center">Frequently Asked Questions (FAQ)</h2>
+    <div class="accordion" id="faqAccordion">
+        <div class="card">
+            <div class="card-header" id="headingOne">
+                <h2 class="mb-0">
+                    <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                        Siapa Nama Asli ADONNET?
+                    </button>
+                </h2>
+            </div>
+            <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#faqAccordion">
+                <div class="card-body">
+                    Nama Asli ADONNET adalah...
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+                <h2 class="mb-0">
+                    <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Siapa Nama Asli ADONNET?
+                    </button>
+                </h2>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#faqAccordion">
+                <div class="card-body">
+                    Nama Asli ADONNET adalah...
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingThree">
+                <h2 class="mb-0">
+                    <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Siapa Nama Asli ADONNET?
+                    </button>
+                </h2>
+            </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#faqAccordion">
+                <div class="card-body">
+                    Nama Asli ADONNET adalah...
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingFour">
+                <h2 class="mb-0">
+                    <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                        Siapa Nama Asli ADONNET?
+                    </button>
+                </h2>
+            </div>
+            <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#faqAccordion">
+                <div class="card-body">
+                    Nama Asli ADONNET adalah...
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include('footer.php'); ?>
