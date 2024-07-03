bd60a9ad7353d1f5ad751566bb3b33eeea78e93c
AdonNeet <baradontap@gmail.com>
2024-07-03 13:10:30 +0700

    chore: backend pinjam

--- public/user/pinjam-isi.php
+++ public/user/pinjam-isi.php
@@ -41,7 +41,91 @@
             </tr>
             <!-- Add more rows as needed -->
           </tbody>
+        </table><?php 
+  include('header.php'); 
+  // Lakukan koneksi ke database sesuai konfigurasi kamu
+  
+  // Ambil id_akun dari session
+  session_start();
+  $id_akun = $_SESSION['id_akun']; // Gantikan dengan nama session yang sesuai
+  
+  // Query untuk mengambil data peminjaman berdasarkan id_akun
+  // Misalnya, jika tabel peminjaman kamu bernama 'peminjaman' dan kolom-kolomnya adalah 'id', 'id_akun', 'judul_buku', dan 'tanggal_pinjam'
+  // Sesuaikan query ini dengan struktur tabel dan database kamu
+  $query = "SELECT * FROM peminjaman WHERE id_akun = $id_akun";
+  
+  // Eksekusi query
+  // $result = mysqli_query($koneksi, $query); // Gantikan $koneksi dengan koneksi database kamu
+
+  // Iterasi hasil query untuk menampilkan data dalam tabel
+  $result = [
+    ['id' => 1, 'judul_buku' => 'Book Title 1', 'tanggal_pinjam' => '2024-06-01'],
+    ['id' => 2, 'judul_buku' => 'Book Title 2', 'tanggal_pinjam' => '2024-06-02'],
+    ['id' => 3, 'judul_buku' => 'Book Title 3', 'tanggal_pinjam' => '2024-06-03'],
+  ]; // Contoh hasil query untuk simulasi
+
+  ?>
+
+<body>
+  <div class="container-fluid">
+    <div class="row mt-5">
+      <div class="col-md-3">
+        <div class="list-group">
+          <a href="profile.php" class="list-group-item list-group-item-action">Akun Saya</a>
+          <a href="#" class="list-group-item list-group-item-action">Pinjaman Saya</a>
+          <a href="/../logout.php" class="list-group-item list-group-item-action">Keluar</a>
+        </div>
+      </div>
+      <div class="col-md-9">
+        <h3>Daftar Pinjaman</h3>
+        <?php if (!empty($result)): ?>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">No.</th>
+              <th scope="col">Buku yang dipinjam</th>
+              <th scope="col">Tanggal pinjam</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($result as $row): ?>
+            <tr>
+              <th scope="row"><?php echo $row['id']; ?></th>
+              <td><?php echo $row['judul_buku']; ?></td>
+              <td><?php echo $row['tanggal_pinjam']; ?></td>
+              <td><button class="btn btn-danger">Kembalikan</button></td>
+            </tr>
+            <?php endforeach; ?>
+          </tbody>
         </table>
+        <?php else: ?>
+          <div class="d-flex justify-content-center align-items-center" style="height: 300px;">
+          <div class="text-center">
+            <div class="row g-2 align-items-center">
+              <div>
+                <img src="empty_order.png" alt="Empty" class="img-fluid" style="height: 300px; width: 300px">
+              </div>
+              <div style="width: 35vh">
+                <p>Kamu Belum Pernah Meminjam Buku</p><br>
+                <p>BuBar memiliki banyak jenis buku yang menunggu untuk dipinjam. </br>Yuk Mulai Pinjam!</p>
+                <button class="btn btn-primary">Mulai Pinjam</button>
+              </div>
+          </div>
+          </div>
+        </div>
+        <?php endif; ?>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+</body>
+</html>
+
       </div>
     </div>
   </div>
--- public/user/pinjam.php
+++ public/user/pinjam.php
@@ -1,30 +1,72 @@
+<?php 
+require_once __DIR__ . '/../../config/database.php';
+require_once __DIR__ . '/../../app/models/Akun.php';
+require_once __DIR__ . '/../../app/models/User.php';
+
+session_start();
+// jika belum login maka diarahkan ke halaman login
+if (!isset($_SESSION["user_id"])) {
+  header("Location: /../auth");
+  exit;
+}
+
+// Mengambil id_akun dari session
+$id_akun = $_SESSION["user_id"];
+
+// Membuat instance dari model Akun dan User
+$akunModel = new Akun();
+$userModel = new User();
+
+// Mengambil data akun dan user berdasarkan id_akun
+$akun = $akunModel->find($id_akun);
+$user = $userModel->findByAkunId($id_akun);
+
+$id_user = $user['id_user'];
+$query = "SELECT * FROM peminjaman WHERE id_user = '$id_user'";
+
+$result = mysqli_query($conn, $query);
+
+?>
+
 <?php include('header.php'); ?>
 <body>
   <div class="container-fluid">
     <div class="row mt-5">
       <div class="col-md-3">
         <div class="list-group">
-          <a href="detail/user.html" class="list-group-item list-group-item-action">Akun Saya</a>
+          <a href="profile.php" class="list-group-item list-group-item-action">Akun Saya</a>
           <a href="#" class="list-group-item list-group-item-action">Pinjaman Saya</a>
-          <a href="#" class="list-group-item list-group-item-action">Keluar</a>
+          <a href="/../logout.php" class="list-group-item list-group-item-action">Keluar</a>
         </div>
       </div>
       <div class="col-md-9">
         <h3>Daftar Pinjaman</h3>
-        <div class="d-flex justify-content-center align-items-center" style="height: 300px;">
-          <div class="text-center">
-            <div class="row g-2 align-items-center">
-              <div>
-                <img src="empty_order.png" alt="Empty" class="img-fluid" style="height: 300px; width: 300px">
-              </div>
-              <div style="width: 35vh">
-                <p>Kamu Belum Pernah Meminjam Buku</p><br>
-                <p>BuBar memiliki banyak jenis buku yang menunggu untuk dipinjam. </br>Yuk Mulai Pinjam!</p>
-                <button class="btn btn-primary">Mulai Pinjam</button>
-              </div>
-          </div>
-          </div>
-        </div>
+        <?php if (mysqli_num_rows($result) > 0): ?>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">No.</th>
+                <th scope="col">Buku yang dipinjam</th>
+                <th scope="col">Tanggal pinjam</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php $nomer = 1; while ($row = mysqli_fetch_assoc($result)): ?>
+                <tr>
+                <form method="POST" action="pinjam.php">
+                  <th scope="row"><?php echo $nomer++; ?></th>
+                  <td><?php echo $row['id_buku']; ?></td>
+                  <td><?php echo $row['tanggal_pinjam']; ?></td>
+                  <td><button type="submit" class="btn btn-danger">Kembalikan</button></td>
+                </form>
+                </tr>
+              <?php endwhile; ?>
+            </tbody>
+          </table>
+        <?php else: ?>
+          <p>Belum ada data peminjaman.</p>
+        <?php endif; ?>
       </div>
     </div>
   </div>
