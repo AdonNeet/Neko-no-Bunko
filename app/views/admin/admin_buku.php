9c6e5c010d8f686334c911a526f2b1697281e308
9Out <nirot.ahmadi2004@gmail.com>
2024-07-03 11:32:32 +0700

    chore add admin.js

--- app/views/admin/admin_buku.php
+++ app/views/admin/admin_buku.php
@@ -1,14 +1,14 @@
 <?php
 
-include 'koneksi.php';
-session_start();
+// include 'koneksi.php';
+// session_start();
 
 // jika bukan admin diarahkan ke halaman user
-if( isset($_SESSION["login"]) ) {
-    if ($_SESSION['id_user'] !== 1) {
-        header("Location: index.php");
-    }
-}
+// if( isset($_SESSION["login"]) ) {
+//     if ($_SESSION['id_user'] !== 1) {
+//         header("Location: index.php");
+//     }
+// }
 
 // Jika tombol tambah di tekan, tampilkan form tambah buku
 // if (isset($_POST['tambah'])) {
@@ -18,70 +18,70 @@ if( isset($_SESSION["login"]) ) {
 // }
 
 // jika belum login maka diarahkan ke halaman login
-if( !isset($_SESSION["login"]) ) {
-    header("Location: login.php");
-    exit;
-}
+// if( !isset($_SESSION["login"]) ) {
+//     header("Location: login.php");
+//     exit;
+// }
 
-if (isset($_POST['add_book'])) {
-    $judul = mysqli_real_escape_string($conn, $_POST['judul']);
-    $penulis = mysqli_real_escape_string($conn, $_POST['penulis']);
-    $penerbit = mysqli_real_escape_string($conn, $_POST['penerbit']);
-    $tahunterbit = mysqli_real_escape_string($conn, $_POST['tahunterbit']);
-    $stok = mysqli_real_escape_string($conn, $_POST['stok']);
-    $keterangan = mysqli_real_escape_string($conn, $_POST['keterangan']);
-    $foto = $_FILES['foto']['name'];
-    $foto_tmp_name = $_FILES['foto']['tmp_name'];
-    $foto_folder = 'uploaded_img/' . $foto;
+// if (isset($_POST['add_book'])) {
+//     $judul = mysqli_real_escape_string($conn, $_POST['judul']);
+//     $penulis = mysqli_real_escape_string($conn, $_POST['penulis']);
+//     $penerbit = mysqli_real_escape_string($conn, $_POST['penerbit']);
+//     $tahunterbit = mysqli_real_escape_string($conn, $_POST['tahunterbit']);
+//     $stok = mysqli_real_escape_string($conn, $_POST['stok']);
+//     $keterangan = mysqli_real_escape_string($conn, $_POST['keterangan']);
+//     $foto = $_FILES['foto']['name'];
+//     $foto_tmp_name = $_FILES['foto']['tmp_name'];
+//     $foto_folder = 'uploaded_img/' . $foto;
 
-    $add_book_query = mysqli_query($conn, "INSERT INTO `buku`(judul, foto, penulis, penerbit, tahunterbit, stok, keterangan) VALUES('$judul', '$foto', '$penulis', '$penerbit', '$tahunterbit', '$stok', '$keterangan')") or die('Query failed');
+//     $add_book_query = mysqli_query($conn, "INSERT INTO `buku`(judul, foto, penulis, penerbit, tahunterbit, stok, keterangan) VALUES('$judul', '$foto', '$penulis', '$penerbit', '$tahunterbit', '$stok', '$keterangan')") or die('Query failed');
 
-    if ($add_book_query) {
-        move_uploaded_file($foto_tmp_name, $foto_folder);
-        setcookie('message', json_encode(['Book added successfully!']), time() + 10, "/");
-    } else {
-        setcookie('message', json_encode(['Book addition failed!']), time() + 10, "/");
-    }
-}
+//     if ($add_book_query) {
+//         move_uploaded_file($foto_tmp_name, $foto_folder);
+//         setcookie('message', json_encode(['Book added successfully!']), time() + 10, "/");
+//     } else {
+//         setcookie('message', json_encode(['Book addition failed!']), time() + 10, "/");
+//     }
+// }
 
-if (isset($_GET['delete'])) {
-    $delete_id = $_GET['delete'];
-    $delete_image_query = mysqli_query($conn, "SELECT foto FROM `buku` WHERE id_buku = '$delete_id'") or die('Query failed');
-    $fetch_delete_image = mysqli_fetch_assoc($delete_image_query);
-    unlink('uploaded_img/' . $fetch_delete_image['foto']);
-    mysqli_query($conn, "DELETE FROM `buku` WHERE id_buku = '$delete_id'") or die('Query failed');
+// if (isset($_GET['delete'])) {
+//     $delete_id = $_GET['delete'];
+//     $delete_image_query = mysqli_query($conn, "SELECT foto FROM `buku` WHERE id_buku = '$delete_id'") or die('Query failed');
+//     $fetch_delete_image = mysqli_fetch_assoc($delete_image_query);
+//     unlink('uploaded_img/' . $fetch_delete_image['foto']);
+//     mysqli_query($conn, "DELETE FROM `buku` WHERE id_buku = '$delete_id'") or die('Query failed');
+//     header('location:admin_buku.php');
+// }
+
+// if (isset($_POST['update_book'])) {
+//     $update_b_id = $_POST['update_b_id'];
+//     $update_judul = mysqli_real_escape_string($conn, $_POST['update_judul']);
+//     $update_penulis = mysqli_real_escape_string($conn, $_POST['update_penulis']);
+//     $update_penerbit = mysqli_real_escape_string($conn, $_POST['update_penerbit']);
+//     $update_tahunterbit = mysqli_real_escape_string($conn, $_POST['update_tahunterbit']);
+//     $update_stok = mysqli_real_escape_string($conn, $_POST['update_stok']);
+//     $update_keterangan = mysqli_real_escape_string($conn, $_POST['update_keterangan']);
+//     $update_foto = $_FILES['update_image']['name'];
+//     $update_foto_tmp_name = $_FILES['update_image']['tmp_name'];
+//     $update_foto_folder = 'uploaded_img/' . $update_foto;
+//     $update_old_image = $_POST['update_old_image'];
+
+//     if (!empty($update_foto)) {
+//         move_uploaded_file($update_foto_tmp_name, $update_foto_folder);
+//         unlink('uploaded_img/' . $update_old_image);
+//         $update_query = mysqli_query($conn, "UPDATE `buku` SET judul = '$update_judul', penulis = '$update_penulis', penerbit = '$update_penerbit', tahunterbit = '$update_tahunterbit', stok = '$update_stok', keterangan = '$update_keterangan', foto = '$update_foto' WHERE id_buku = '$update_b_id'") or die('Query failed');
+//     } else {
+//         $update_query = mysqli_query($conn, "UPDATE `buku` SET judul = '$update_judul', penulis = '$update_penulis', penerbit = '$update_penerbit', tahunterbit = '$update_tahunterbit', stok = '$update_stok', keterangan = '$update_keterangan' WHERE id_buku = '$update_b_id'") or die('Query failed');
+//     }
+
+//     if ($update_query) {
+//         setcookie('message', json_encode(['Book updated successfully!']), time() + 10, "/");
+//     } else {
+//         setcookie('message', json_encode(['Book update failed!']), time() + 10, "/");
+//     }
+
     header('location:admin_buku.php');
-}
-
-if (isset($_POST['update_book'])) {
-    $update_b_id = $_POST['update_b_id'];
-    $update_judul = mysqli_real_escape_string($conn, $_POST['update_judul']);
-    $update_penulis = mysqli_real_escape_string($conn, $_POST['update_penulis']);
-    $update_penerbit = mysqli_real_escape_string($conn, $_POST['update_penerbit']);
-    $update_tahunterbit = mysqli_real_escape_string($conn, $_POST['update_tahunterbit']);
-    $update_stok = mysqli_real_escape_string($conn, $_POST['update_stok']);
-    $update_keterangan = mysqli_real_escape_string($conn, $_POST['update_keterangan']);
-    $update_foto = $_FILES['update_image']['name'];
-    $update_foto_tmp_name = $_FILES['update_image']['tmp_name'];
-    $update_foto_folder = 'uploaded_img/' . $update_foto;
-    $update_old_image = $_POST['update_old_image'];
-
-    if (!empty($update_foto)) {
-        move_uploaded_file($update_foto_tmp_name, $update_foto_folder);
-        unlink('uploaded_img/' . $update_old_image);
-        $update_query = mysqli_query($conn, "UPDATE `buku` SET judul = '$update_judul', penulis = '$update_penulis', penerbit = '$update_penerbit', tahunterbit = '$update_tahunterbit', stok = '$update_stok', keterangan = '$update_keterangan', foto = '$update_foto' WHERE id_buku = '$update_b_id'") or die('Query failed');
-    } else {
-        $update_query = mysqli_query($conn, "UPDATE `buku` SET judul = '$update_judul', penulis = '$update_penulis', penerbit = '$update_penerbit', tahunterbit = '$update_tahunterbit', stok = '$update_stok', keterangan = '$update_keterangan' WHERE id_buku = '$update_b_id'") or die('Query failed');
-    }
-
-    if ($update_query) {
-        setcookie('message', json_encode(['Book updated successfully!']), time() + 10, "/");
-    } else {
-        setcookie('message', json_encode(['Book update failed!']), time() + 10, "/");
-    }
-
-    header('location:admin_buku.php');
-}
+// }
 ?>
 
 <!DOCTYPE html>
@@ -120,7 +120,7 @@ if (isset($_POST['update_book'])) {
     </div>
     <div class="box-container">
     <?php
-    $select_books = mysqli_query($conn, "SELECT * FROM `buku`ORDER BY id_buku DESC") or die('Query failed');
+    // $select_books = mysqli_query($conn, "SELECT * FROM `buku`ORDER BY id_buku DESC") or die('Query failed');
     if (mysqli_num_rows($select_books) > 0) {
         while ($fetch_books = mysqli_fetch_assoc($select_books)) {
     ?>
@@ -203,10 +203,10 @@ if (isset($_POST['update_book'])) {
             while ($fetch_update = mysqli_fetch_assoc($update_query)) {
     ?>
     <form action="admin_buku.php" method="post" enctype="multipart/form-data">
-        <input type="hidden" name="update_b_id" value="<?php echo $fetch_update['id_buku']; ?>">
-        <input type="hidden" name="update_old_image" value="<?php echo $fetch_update['foto']; ?>">
+        <!-- <input type="hidden" name="update_b_id" value="<?php echo $fetch_update['id_buku']; ?>">
+        <input type="hidden" name="update_old_image" value="<?php echo $fetch_update['foto']; ?>"> -->
         <!-- <img src="uploaded_img/<?php echo $fetch_update['foto']; ?>" alt=""> -->
-        <input type="text" name="update_judul" value="<?php echo $fetch_update['judul']; ?>" class="box" required placeholder="Enter book title">
+        <!-- <input type="text" name="update_judul" value="<?php echo $fetch_update['judul']; ?>" class="box" required placeholder="Enter book title">
         <input type="text" name="update_penulis" value="<?php echo $fetch_update['penulis']; ?>" class="box" required placeholder="Enter author name">
         <input type="text" name="update_penerbit" value="<?php echo $fetch_update['penerbit']; ?>" class="box" required placeholder="Enter publisher name">
         <input type="date" name="update_tahunterbit" value="<?php echo $fetch_update['tahunterbit']; ?>" class="box" required placeholder="Enter year of publication">
@@ -215,7 +215,7 @@ if (isset($_POST['update_book'])) {
         <input type="file" class="box" name="update_image" accept="image/jpg, image/jpeg, image/png">
         <input type="file" name="ebook" accept="application/pdf" class="box" placeholder="upload foto">
         <input type="submit" value="Update" name="update_book" class="option-btn">
-        <input type="button" value="Cancel" id="close-update" class="delete-btn" onclick="window.location.href='admin_buku.php';">
+        <input type="button" value="Cancel" id="close-update" class="delete-btn" onclick="window.location.href='admin_buku.php';"> -->
     </form>
     <?php
             }
